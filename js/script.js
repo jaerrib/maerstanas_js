bdb7c4c47b23b00bd6478c42ae64716dd6f05166
jaerrib <jaerib@tutanota.com>
2023-02-12 05:20:40 -0500

    Fix various style issues - Replace var declarations with let or const - Replace == with === for comparisons

--- js/script.js
+++ js/script.js
@@ -45,9 +45,9 @@ function createBoard() {
     const edge = 3;
 
     // Creates a 9x9 grid for the board
-    for (var rowNum = 0; rowNum < size; rowNum++) {
-        var row = [];
-        for (var colNum = 0; colNum < size; colNum++) {
+    for (let rowNum = 0; rowNum < size; rowNum++) {
+        let row = [];
+        for (let colNum = 0; colNum < size; colNum++) {
             row.push(empty);
         }
         board.push(row);
@@ -55,12 +55,12 @@ function createBoard() {
 
     /* Assigns edge positions to the relevant array locations for use board position
     analysis */
-    for (var colNum = 0; colNum < size; colNum++) {
+    for (let colNum = 0; colNum < size; colNum++) {
         board[0][colNum] = edge;
         board[8][colNum] = edge;
     }
 
-    for (var rowNum = 1; rowNum < size - 1; rowNum++) {
+    for (let rowNum = 1; rowNum < size - 1; rowNum++) {
         board[rowNum][0] = edge;
         board[rowNum][8] = edge;
     }
@@ -69,7 +69,7 @@ function createBoard() {
 }
 
 function changeActivePlayer() {
-    if (activePlayer == 1) {
+    if (activePlayer === 1) {
         activePlayer = 2;
     }
     else {
@@ -84,32 +84,64 @@ function updateScore() {
 
 function convertToRowNum(letter) {
     // Converts player's letter input to a number usable by various functions.
-    possibleRows = ["A", "B", "C", "D", "E", "F", "G"];
-    for (var i = 0; i < possibleRows.length; i++) {
-        if (letter == possibleRows[i]) {
+    const possibleRows = ["A", "B", "C", "D", "E", "F", "G"];
+    for (let i = 0; i < possibleRows.length; i++) {
+        if (letter === possibleRows[i]) {
             return i + 1
         }
     }
 }
 
+function findAdjacent(row, col) {
+    // Returns assigned value for positions adjacent to a given board position
+    let adjacentPositions = [
+        [row - 1, col],
+        [row, col - 1],
+        [row, col + 1],
+        [row + 1, col]
+    ];
+    console.log(adjacentPositions)
+    return adjacentPositions
+}
+
+
+function hasFourHinges(row, col) {
+
+    // Evaluates the number of hinges a player move would have
+    // if played at a given board position
+    let adjacentPositions = findAdjacent(row, col)
+    // Counts the number of hinges a potential position would have
+    let hinges = 0;
+    for (let pos = 0; pos < adjacentPositions.length; pos++) {
+        let rowToCheck = adjacentPositions[pos][1]
+        let positionCheck = board[row][col]
+        if (positionCheck !== 0) {
+            hinges += 1;
+        }
+    }
+    return hinges > 3;
+}
+
 function assignBoardPos(row, col) {
     board[row][col] = activePlayer;
-    console.log(activePlayer)
+    // console.log(activePlayer)
     score[activePlayer - 1]++
 }
 
 function isValidMove(row, col) {
-    console.log(board[row][col])
-    if (board[row][col] == 0) {
-        return true;
+    // console.log(board[row][col])
+    if (board[row][col] === 0) {
+        if (!hasFourHinges(row, col)){
+            return true;
+        }
     }
     return false
 }
 
 function placeStone(location) {
-    var locArr = Array.from(location.id);
-    var rowNum = convertToRowNum(locArr[0]);
-    var colNum = locArr[1];
+    let locArr = Array.from(location.id);
+    let rowNum = convertToRowNum(locArr[0]);
+    let colNum = locArr[1];
     if (isValidMove(rowNum, colNum)) {
         assignBoardPos(rowNum, colNum)
         location.innerText = "●";
